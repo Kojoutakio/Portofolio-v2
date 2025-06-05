f129fe8ce324989d98783d75ec34377b62783c06
Kojouta <earthkun03@gmail.com>
2025-06-05 18:35:31 +0700

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -20,13 +20,13 @@ export default {
           id: 3,
           name: 'BLENDER',
           imageUrl: 'https://files.catbox.moe/f7ppsy.png',
-          status: 'Profesional'
+          status: 'Advanced'
         },
         {
           id: 4,
           name: 'Javascript',
           imageUrl: 'https://cdn.icon-icons.com/icons2/2415/PNG/512/javascript_original_logo_icon_146455.png',
-          status: 'Advance'
+          status: 'Beginner'
         },
         {
           id: 5,
@@ -128,16 +128,15 @@ export default {
         </div>
         <div class="md:w-7/12">
           <p class="mb-3 md:mb-7 fadein-left fadeins-1">
-            &nbsp; &nbsp; &nbsp; Hi everyone! My name is Kojouta. I'm a web developer And 3D model artist from depok, West Java. I
-            have 1 year of experience in back-end web development.
-            I really enjoy what I do right now, in my opinion, creating programs is not just a job, but also an art that
+            &nbsp; &nbsp; &nbsp; Hi everyone! My name is Kojouta. I'm a web developer And 3D model artist from Jakarta, West Java. I
+            have 1 year of experience in in 3D modeling and Animation.
+            I really enjoy what I do right now, in my opinion, making 3D modeling and Animation is not just a job, but also an art that
             has
             aesthetic value
           </p>
           <p class="mb-3 fadein-left fadeins-2">
-            &nbsp; &nbsp; &nbsp; My job is to build your website to be functional and user-friendly yet still attractive.
-            In addition, I provide a personal touch to your product and ensure that the website catches attention and is
-            easy to use.
+            &nbsp; &nbsp; &nbsp; My job is to help you in 3D modeling.
+            In addition, I give some details to your 3D modeling and make sure that the Model is optimized and avoids lag so that the game runs smoothly.
             My goal is to convey your message and identity in the most creative way. If you are interested in hiring me,
             please contact the listed contact.
           </p>
