0111093169dfd63df1c0ecaccf88a29d0ff112ca
Kojouta <earthkun03@gmail.com>
2025-07-07 01:52:23 +0700

    Update PortfolioView.vue

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -42,10 +42,10 @@ export default {
         },
         {
           id: 5,
-          name: 'Malo 1471',
-          imageUrl: 'Malo',
-          status: 'I took the reference from scp 1471. Actually, I dont make character styles like this anymore [focus more on low poly characters] and thats a model that Ive made quite well..',
-          tech: 'BLender, SKetch',
+          name: 'TV Girl',
+          imageUrl: 'Polylow',
+          status: 'I took the reference from the sketch on Pinterest and this is also a low poly character that I made. Disclaimer: this is not skibidi',
+          tech: 'BLender,SKetch,Pinterest',
           github: 'null',
           demo: 'null'
         }
